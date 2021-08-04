32a73b771052d37fc1b5575465c6701f00346fde
박동현 <donghyeondev@gmail.com>
2021-08-04 18:03:23 +0900

    로그인 테스트를 위한 코드작성

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import { Route, Link } from 'react-router-dom';
 
 function App() {
     const [res, setRes] = useState(null);
+    const headers = {
+        'Authorization' : localStorage.getItem("token")
+    };
 
     const goToBlizzard = async () => {
         console.log('before =' + res);
@@ -16,14 +19,17 @@ function App() {
         console.log('after =' + res);
     };
 
-    const login = async () => {
+    const login = () => {
         const data = {
             email: 'donghyeondev@gmail.com',
             password: 'qkrgus0712!',
         };
-        const response = await axios.get(
-            'https://kr.battle.net/oauth/authorize?:region=kr&response_type=code&client_id=cd5f2cc20f0e4be08e31ae9938e56b2d&redirect_uri=http://localhost:8880/login/oauth&scope=wow.profile'
-        );
+        const response = axios.post(
+            'http://localhost:8880/login',data,{headers}
+        ).then(res => {
+            localStorage.setItem("token",res.data.data.accessToken);
+            console.log(res.data.data.accessToken);
+        });
 
         console.log(response);
     };
@@ -37,12 +43,17 @@ function App() {
     };
     return (
         <div className="App">
+            <div>
+                <button type="button" onClick={login} >로그인</button>
+            </div>
+
             <a href="https://kr.battle.net/oauth/authorize?:region=kr&response_type=code&client_id=cd5f2cc20f0e4be08e31ae9938e56b2d&redirect_uri=http://localhost:3000/oauth&scope=wow.profile">
                 블리자드계정연동
             </a>
             <div>
                 <a href="http://localhost:3000">HOME</a>
             </div>
+
             <Route path="/oauth" component={Oauth} />
         </div>
 
